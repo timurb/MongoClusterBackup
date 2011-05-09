23762ca704f3faffe45a08c2b5592b5be0c8e439
Timur Batyrshin <timur.batyrshin@flatsoft.com>
2011-05-09 12:21:05 +0400

    added mongo safe connection class

--- lib/core_ext/mongo.rb
+++ lib/core_ext/mongo.rb
@@ -5,6 +5,7 @@ require 'rubygems'
 require 'mongo'
 
 module Mongo
+
   class MongosConnection < ::Mongo::Connection
     def stop_balancer
       self['config']['settings'].update( { :_id => "balancer" }, { :stopped => true } )
@@ -18,4 +19,24 @@ module Mongo
       self['admin'].command( { :listShards => 1} )["shards"]
     end
   end
+
+
+  #     patching the Connection object's lock! and unlock! methods to be safe to run
+  class Connection
+    alias :unsafe_lock! :lock!
+    alias :unsafe_unlock! :unlock!
+
+    def lock!(*args)
+      unsafe_lock!(*args) unless self.locked?
+    end
+
+    def unlock!(*args)
+      begin
+        unsafe_unlock!(*args)
+      rescue => e
+        e
+      end
+    end
+  end
+
 end
---
+++ test/mongo_connection_test.rb
@@ -0,0 +1,51 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+$:.unshift File.join(File.dirname(__FILE__),'..','lib')
+
+require 'test/unit'
+require 'core_ext/mongo'
+
+# => stub class for tests
+module Mongo
+  class TestConnection < Connection
+    attr_accessor :lock_was_run
+    def initialize
+      @lock_was_run = false
+      @locked = false
+    end
+    
+    def unsafe_lock!
+      @locked = true
+      @lock_was_run = true
+    end
+    
+    def locked?
+      @locked
+    end
+    
+    def unsafe_unlock!
+      raise RuntimeError, "This should not be raised"
+    end
+  end
+end
+
+class MongoConnectionTest < Test::Unit::TestCase
+  def setup
+    @m=Mongo::TestConnection.new
+  end
+
+  def test_dont_run_lock_when_locked
+    @m.lock!
+    assert @m.locked?
+    @m.lock_was_run = false
+    @m.lock!
+    assert !@m.lock_was_run
+  end
+
+  def test_rescue_during_unlock
+    assert_nothing_raised do
+      @m.unlock!
+    end
+  end
+end
