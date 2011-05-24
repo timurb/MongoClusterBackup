a6a78ed4361a621d0170a05d0ec16c81371e6a4e
Timur Batyrshin <timur.batyrshin@flatsoft.com>
2011-05-24 17:41:42 +0400

    added whole bunch of things to mongodump runner

--- lib/mongodump_backup_runner.rb
+++ lib/mongodump_backup_runner.rb
@@ -4,9 +4,27 @@ require 'abstract_backup_runner'
 module MongoBackup
   class MongoDumpBackupRunner < AbstractBackupRunner
 
-    def backup_node(node)
-      system( 'mongodump', '-o', node.shard_name, '-h', node.host, '--port', node.port.to_s )
+    def initialize(*args)
+      super
+      @opts[:backup_path] = File.join( ( @opts[:backup_dir] || Dir.pwd ), @backup_id )
+      @stdout = STDOUT.dup
+      @null = File.open('/dev/null','w')
     end
+
+    def backup_node(node)
+      begin
+        STDOUT.reopen(@null)   unless @opts[:verbose]
+        system( 'mongodump', '-o', File.join(@opts[:backup_path],node.shard_name), '-h', node.host, '--port', node.port.to_s )
+      ensure
+        begin
+          STDOUT.reopen(@stdout)  unless @opts[:verbose]
+        rescue => e
+          STDOUT.reopen(STDERR)
+          raise e
+        end
+      end
+    end
+
     def update_waiting
       @waiting = []
     end
